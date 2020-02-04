83d4ac8933f3f439b089ceafc89d3ac98d013b0a
10647421 <10647421@INFPHSZA07218.lntinfotech.com>
2020-02-04 20:43:20 +0530

    updated

--- src/test/java/projects/fox/api/tests/getrequest.java
+++ src/test/java/projects/fox/api/tests/getrequest.java
@@ -1,30 +1,37 @@
 package projects.fox.api.tests;
 
+import static io.restassured.RestAssured.given;
+
+import java.net.URISyntaxException;
 
 import org.testng.annotations.Test;
-import io.restassured.response.ValidatableResponse;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import io.restassured.RestAssured;
 
 public class getrequest {
 
-	ValidatableResponse respone;
-
 	@Test(enabled = true, priority = 0, description = "getrequest")
-	public void test1() {
+	public void test1() throws URISyntaxException {
 
-		respone = given().
-				when().
-				get("http://dedmsapi.centralus.cloudapp.azure.com:8084/country-codes?CountryCode=95,99&Year=2012,2011,2013,2014,2015,2016,2017,2018,2019").
-				then();
-		
+		/*
+		 * Rest Assured coding stanbdard for get put post delete request
+		 * 
+		 * given() //block 1 Request headers Parameters request cookies
+		 * 
+		 * when() //block 2 get(resource) post(resource) put(resources)
+		 * 
+		 * 
+		 * then() //block3 assertion to make sure we are getting correct response
+		 * 
+		 * 
+		 * extract() //block 4 to extract response body
+		 */
 
+		RestAssured.baseURI = "http://dedmsapi.centralus.cloudapp.azure.com:8084";
 
-		// Assert response code
-		respone.statusCode(200);
-		
-		//respone.body(arguments, responseAwareMatcher)
+		given().param("CountryCode", "95,99").param("Year", "2012,2011,2013,2014,2015,2016,2017,2018,2019").when()
+				.get("/country-codes").then().assertThat().statusCode(200);
+
 	}
 
 }
--- src/test/java/projects/fox/pages/foxLoginPage.java
+++ src/test/java/projects/fox/pages/foxLoginPage.java
@@ -2,69 +2,62 @@ package projects.fox.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
 import com.baseproject.utility.BasePage;
 
+public class foxLoginPage extends BasePage {
 
+	@FindBy(id = "txtCorpID")
+	WebElement company;
+	@FindBy(id = "txtLoginID")
+	WebElement Username;
+	@FindBy(name = "txtPassword")
+	WebElement Password;
+	@FindBy(name = "btnLogin")
+	WebElement Login;
 
-public class foxLoginPage extends BasePage{
-      
-	  @FindBy(id = "txtCorpID") WebElement company;
-      @FindBy(id = "txtLoginID") WebElement Username;
-      @FindBy(name = "txtPassword") WebElement Password;
-      @FindBy(name = "btnLogin") WebElement Login;
-     
-      
-      
-      public foxLoginPage() {
-            initPageFactory( this);
-      }
-      
-      /*
-     * open url
-     */
-     public void openLoginPage(String url) {
-                       openUrl(url);
-     }
-    
-     /*
-      * Enter Company
-      * Enter Username
-      * Enter Password
-      * CLick on Login Button
-      */
-      
-     public void LoginfoxApplication(String company,String username,String password){
-         
-	     enterCompany(company);
-         enterUsername(username);
-         enterPassword(password);
-         clickOnLogin();
-         
-   }
-     
-     private void enterCompany(String optionText) {
-    	 clickOnElement("Company", company);
-         enterText("Company", company, optionText);
-   }
-      
-      private void enterUsername(String optionText) {
-    	  clickOnElement("User Name", Username);
-            enterText("User Name", Username, optionText);
-      }
-      
-      
-      private void enterPassword(String optionText) {
-    	  clickOnElement("Password", Password);
-            enterText("Password", Password, optionText);
-      }
-      
-      
-      private void clickOnLogin() {
-            clickOnElement("Submit", Login);
-      }
-      
-      
-      
-     
-      
+	// Constructor
+	public foxLoginPage() {
+		initPageFactory(this);
+	}
+
+	/*
+	 * open url
+	 */
+	public void openLoginPage(String url) {
+		openUrl(url);
+	}
+
+	/*
+	 * Enter Company Enter Username Enter Password CLick on Login Button
+	 */
+
+	public void LoginfoxApplication(String company, String username, String password) {
+
+		enterCompany(company);
+		enterUsername(username);
+		enterPassword(password);
+		clickOnLogin();
+
+	}
+
+	private void enterCompany(String optionText) {
+		clickOnElement("Company", company);
+		enterText("Company", company, optionText);
+	}
+
+	private void enterUsername(String optionText) {
+		clickOnElement("User Name", Username);
+		enterText("User Name", Username, optionText);
+	}
+
+	private void enterPassword(String optionText) {
+		clickOnElement("Password", Password);
+		enterText("Password", Password, optionText);
+	}
+
+	private void clickOnLogin() {
+		clickOnElement("Submit", Login);
+	}
+
 }
